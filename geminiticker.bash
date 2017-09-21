f70fd57fa0e0676302aa23a47a5c37052f6e8f91
Henry Romp <henry@tb.deb.net>
2017-09-21 14:50:40 -0400

    init commit

---
+++ geminiticker.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+gprice="https://api.gemini.com/v1/pubticker/btcusd"
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+cyan=$(tput setaf 6)
+while true; do
+
+	read last < <(curl "$gprice" 2> /dev/null | jshon -e last) 
+	cmp=${last//[\".]} 
+	if (( prev == cmp ))  
+	then printf %s "$cyan"  
+	elif (( prev < cmp ))  
+	then printf %s "$green"
+	else printf %s "$red"; 
+	fi 
+	clear
+	printf "BTCUSD:\\n"
+	printf "${last//\"/}"
+	sleep 3 
+        prev=$cmp	
+	
+done
