50a793255c02d96a1a269c0e8f7fb576195c55ef
Henry Romp <henry@tb.deb.net>
2017-09-25 10:15:12 -0400

    add eth

--- geminiticker.bash
+++ geminiticker.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 gprice="https://api.gemini.com/v1/pubticker/btcusd"
+gethprice="https://api.gemini.com/v1/pubticker/ethusd"
+tput bold
 tput civis
 red=$(tput setaf 1)
 green=$(tput setaf 2)
@@ -14,11 +16,23 @@ while true; do
 	elif (( prev < cmp ))  
 	then printf %s "$green"
 	else printf %s "$red"; 
-	fi 
-	clear
-	printf "BTCUSD:\\n"
-	printf "${last//\"/}"
+	fi 	
+	printf "Gemini BTCUSD:\\n"
+	printf "\$""${last//\"/}"
+	printf "\n"
+	read last < <(curl "$gethprice" 2> /dev/null | jshon -e last)
+	cmp2=${last//[\".]}
+	if (( prev2 == cmp2 ))
+	then printf %s "$cyan"
+	elif (( prev2 < cmp2 ))
+	then printf %s "$green"
+	else printf %s "$red";
+	fi
+	printf "Gemini ETHUSD:\\n"
+	printf "\$""${last//\"/}"	
 	sleep 3 
+	clear
+	prev2=$cmp2
         prev=$cmp	
 	
 done
